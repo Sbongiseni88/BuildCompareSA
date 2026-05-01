287cf0952f96707e67ec40359416224c723bdd03
Sibongiseni Dubazane <sibongiseni.dubazane@safetyio.com>
2026-05-01 13:40:20 +0200

    fix: restore supabase getUser check

--- buildcompare-sa/src/utils/supabase/middleware.ts
+++ buildcompare-sa/src/utils/supabase/middleware.ts
@@ -41,9 +41,9 @@ export async function updateSession(request: NextRequest) {
     // supabase.auth.getUser(). A simple mistake could make it very hard to debug
     // issues with users being randomly logged out.
 
-    // const {
-    //     data: { user },
-    // } = await supabase.auth.getUser()
+    const {
+        data: { user },
+    } = await supabase.auth.getUser()
 
     // if (
     //     !user &&
